f76de4573f726fbc92d3c4ec1f08b756c52bc30a
Ahmed.Sherif <ahmed.sherif@integrant.com>
2019-07-28 19:26:35 +0200

    Add Login Screen

---
+++ eShopApp/eShopApp/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using eShopApp.Models;
+
+namespace eShopApp.Services
+{
+    public class UserService : IUserService
+    {
+        ObservableCollection<User> Users = new ObservableCollection<User>
+        {
+            new User
+            {
+                Id = 1,
+                Username = "ahmed"
+            },
+            new User
+            {
+                Id = 2,
+                Username = "basma"
+            },
+            new User
+            {
+                Id = 3,
+                Username = "youssef"
+            }
+        };
+
+        public ICollection<User> GetUsers()
+        {
+            return Users;
+        }
+
+        public bool IsUserExists(string username)
+        {
+            return Users.Any(u=>u.Username == username);
+        }
+    }
+}
---
+++ eShopApp/eShopApp/Views/ProductsPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace eShopApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProductsPage : ContentPage
+    {
+        public ProductsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
