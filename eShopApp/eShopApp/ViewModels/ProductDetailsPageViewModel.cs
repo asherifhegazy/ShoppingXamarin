83a40981bc0f73af883ae881407481854fb5c30c
Ahmed.Sherif <ahmed.sherif@integrant.com>
2019-07-30 13:09:17 +0200

    - edit navbar styling a little bit - update product details View and ViewModel - still needs to Add a working Carousel View

--- eShopApp/eShopApp/ViewModels/ProductDetailsPageViewModel.cs
+++ eShopApp/eShopApp/ViewModels/ProductDetailsPageViewModel.cs
@@ -3,6 +3,8 @@ using eShopApp.Services;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace eShopApp.ViewModels
 {
@@ -13,12 +15,37 @@ namespace eShopApp.ViewModels
         private readonly IProductService _productService;
         private readonly IPageService _pageService;
 
+        string stepper;
+
+        public string Stepper
+        {
+            get => stepper;
+
+            set
+            {
+                if (stepper != value)
+                {
+                    SetValue(ref stepper, value);
+                    OnPropertyChanged(nameof(Stepper));
+                }
+            }
+        }
+
+        public ICommand AddToCartCommand { get; set; }
+
         public ProductDetailsPageViewModel(int productId, IProductService productService, IPageService pageService)
         {
             _productService = productService;
             _pageService = pageService;
 
             Product = _productService.GetProductById(productId);
+
+            AddToCartCommand = new Command(OnAddToCartCommand);
+        }
+
+        private void OnAddToCartCommand()
+        {
+            _pageService.DisplayAlert("Add To Cart",$"{Product.Name}","OK","CANCEL");
         }
     }
 }
